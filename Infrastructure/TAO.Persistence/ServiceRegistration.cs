d20d5ff67a5f2b8b233232250aad0cd6cdb41c4a
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-04-24 14:49:28 +0300

    adding ioc

--- Infrastructure/TAO.Persistence/ServiceRegistration.cs
+++ Infrastructure/TAO.Persistence/ServiceRegistration.cs
@@ -5,8 +5,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TAO.Application.Repositories;
 using TAO.Persistence.Configurations;
 using TAO.Persistence.Contexts;
+using TAO.Persistence.Repositories;
 
 namespace TAO.Persistence
 {
@@ -17,7 +19,20 @@ namespace TAO.Persistence
 
            
             services.AddDbContext<TaoDbContext>(options => options.UseSqlServer(Configurations.Configuration.ConnectionString));
-            
+
+
+            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+
+
+
         }
     }
 }
